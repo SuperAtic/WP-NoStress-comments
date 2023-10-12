caf94066d43810641a877a7dd73223d5c9336a10
SiNteTiKo <SuperAtic@users.noreply.github.com>
2023-10-12 10:50:43 +0200

    Update wp-nostress-comments.php

--- wp-nostress-comments.php
+++ wp-nostress-comments.php
@@ -30,12 +30,22 @@ add_action('wp_enqueue_scripts', 'wp_nostress_enqueue_scripts');
 
 // Replace the default WordPress comments template with the NOSTR comment form.
 function wp_nostress_comment_form($file) {
+    // Check if comments are open and if threaded comments are enabled.
     if(is_singular() && comments_open() && get_option('thread_comments')) {
         // The div where the NOSTR comment form will be initialized.
         echo '<div id="wp-nostress-comments"></div>';
+        echo '<script>
+            // Initialize the NOSTR comment form.
+            NoComment.init({
+                target: document.getElementById("wp-nostress-comments"),
+                // Other initialization parameters...
+            });
+        </script>';
+        // Return our custom comment form to override the default one.
+        return dirname(__FILE__) . '/custom-comments-template.php';
     }
-    return; // Return nothing to ensure the default WP comments don't appear.
+    return $file;
 }
-add_filter('comments_template', 'wp_nostress_comment_form');
+add_filter('comments_template', 'wp_nostress_comment_form', 99); // Use a high priority to ensure our filter runs last.
 
 ?>
